895d3b4357cf0f1f048f88bd230c530df834bc37
yichenp0104 <liyichen20000104@outlook.com>
2021-10-08 17:39:13 -0400

    Update RegisterPage

--- screens/ContentScreens/RegisterPage.js
+++ screens/ContentScreens/RegisterPage.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useCallback } from "react";
-import { Text, View, StyleSheet, Button, Dimensions } from "react-native";
-import SomeComponent from "../../Components/SampleComponents/SomeComponents";
+import React, { useEffect, useState, useCallback } from "react";
+import { Text, TextInput, View, StyleSheet, Button, Dimensions, TouchableOpacity} from "react-native";
 import MeetUpNavBar from "../../Components/MeetUpNavBar";
 import AutoResizableWindow from "../../Components/PageStyling/AutoResizableWindow";
-import { useState } from "react";
+import THEME_COLOR from "../../Constants/Color";
 
 const RegisterPage = param => {
 
@@ -33,25 +32,81 @@ const RegisterPage = param => {
         };
     });
 
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [re_enter_password, setReEnterPassword] = useState('');
+
+    function validateForm() {
+        return email.length > 0 
+        && username.length > 0 
+        && password.length > 0
+        && password == re_enter_password;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+
 
     const MainContent = () => {
         return (
             <View>
                 <MeetUpNavBar navigation={param.navigation}></MeetUpNavBar>
                 <View style={styles.centered}>
-                <Button
-                    title="BACK"
-                    onPress={() => {
-                        param.navigation.goBack()
-                    }}
-                />
-                <View style={{width:'800px', alignItems:"center"}}>
-                    <Text>This is register page</Text>
-                    <Text>Info passed from last page:</Text>
-                    <Text>{fetched_param}</Text>
+                <Text style={{fontSize: 30}}>Please sign-up</Text>
+                <View>
+                    <TextInput style={styles.userInput}
+                        placeholder="Email"
+                        onChangeText={email => setEmail(email)}
+                        defaultValue={email}
+                    />
                 </View>
-                
-                <SomeComponent/>
+                <View>
+                    <TextInput style={styles.userInput}
+                        placeholder="Username"
+                        onChangeText={username => setUsername(username)}
+                        defaultValue={username}
+                    />
+                </View>
+                <View>
+                    <TextInput
+                        style={styles.userInput}
+                        placeholder="Password"
+                        secureTextEntry={true}
+                        onChangeText={(password) => setPassword(password)}
+                    />
+                </View>
+                <View>
+                    <TextInput
+                        style={styles.userInput}
+                        placeholder="Re-enter password"
+                        secureTextEntry={true}
+                        onChangeText={(re_enter_password) => setReEnterPassword(re_enter_password)}
+                    />
+                </View>
+                <TouchableOpacity onPress={() => param.navigation.navigate("ForgotPasswordPage")}>
+                    <Text style={styles.forgot_button}>Terms and conditions</Text>
+                </TouchableOpacity>
+
+                <View style={styles.backbtn}>
+                    <Button 
+                        title="SIGN UP"
+                        color={THEME_COLOR.main}
+                        onPress={() => {
+                            param.navigation.goBack()
+                        }}
+                    />
+                </View>
+                    
+                <View style={styles.backbtn}>
+                    <Button 
+                        title="BACK"
+                        onPress={() => {
+                            param.navigation.goBack()
+                        }}
+                    />
+                </View>
                 </View>
             </View>
         )
@@ -66,9 +121,28 @@ const RegisterPage = param => {
 
 const styles = StyleSheet.create({
     centered: {
-        flex: 1,
+        marginTop: 20,
         justifyContent: "center",
         alignItems: "center",
+    },
+    userInput: {
+        borderRadius: 10,
+        borderColor: THEME_COLOR.subcolor,
+        borderWidth: 1,
+        height: 45,
+        marginTop: 12,
+        padding: 10,
+        alignSelf: "center",
+        width: '150%'
+    },
+    backbtn: {
+        marginTop: 30,
+        width: '25%'
+    },
+    forgot_button: {
+        height: 30,
+        marginTop: 12,
+        textDecorationLine: 'underline'
     },
 })
 
