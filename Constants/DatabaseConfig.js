d616abb8674ae1b9e62d79b4a735b9b444ae5e25
JackHKJ <kjhe217@gmail.com>
2021-10-15 16:49:59 -0400

    DB Module Prototype

--- Constants/DatabaseConfig.js
+++ Constants/DatabaseConfig.js
@@ -3,10 +3,13 @@
 
 export const DATABASE_HOST = "http://us-la-cn2-1.natfrp.cloud:23553"
 
-
-
+// The Building Section
 export const DATABASE_GET_BUILDING_INFO_URL =
     DATABASE_HOST + "/list_building?page=1&page_size=10&name=";
+
+export const DATABASE_GET_LIST_ALL_BUILDINGS =
+    DATABASE_HOST + "/list-all-buildings?name="
+
 
 export const DATABASE_GET_BUILDING_INFO_URL_GENERATOR = (
     pageNum = 1,
--- Models/DatabaseRelated/BuildingDBHandler.js
+++ Models/DatabaseRelated/BuildingDBHandler.js
@@ -1,8 +1,52 @@
+import { DATABASE_GET_LIST_ALL_BUILDINGS } from "../../Constants/DatabaseConfig";
+
 class BuildingDBHandler {
     constructor() {
       
     }
+
+    getAllBuilding = () => {
+        return async (dispatch) => {
+            console.log("fetching all building data");
+            // any async code you want!
+            try {
+                console.log("Before response");
+    
+                let defaultPageSize = 10
+                let defaultPageNum =1
+    
+                const response = await fetch(
+                    DATABASE_GET_BUILDING_INFO_URL_GENERATOR()
+                    // `http://us-la-cn2-1.natfrp.cloud:23553/list-building?page=${defaultPageNum}&page-size=${defaultPageSize}&name=`,
+                    // DATABASE_GET_BUILDING_INFO_URL,
+                    ,{
+                        mode: "cors",
+                        method: "GET",
+                        headers:{
+                            'Content-Type': 'application/json',
+                            'X-Authorization':"WanNeng"
+                        }
+                    }
+                );
+                console.log("After response");
+                console.log(response);
+    
+                if (!response.ok) {
+                    throw new Error("Something went wrong!");
+                }
+    
+                const resData = await response.json();
+                console.log(resData);
+    
+                dispatch({ type: FETCH_AND_UPDATE_LOGIN_STATUS });
+            } catch (err) {
+                // send to custom analytics server
+                throw err;
+            }
+        };
+    };
+    
   }
   
   export default BuildingDBHandler;
-  
+ 
