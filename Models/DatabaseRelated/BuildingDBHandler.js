ce5718ea06af0fa1c1fd5c5618beace375cfa7dd
JackHKJ <kjhe217@gmail.com>
2021-10-15 17:47:23 -0400

    General layout Updated for DB

--- Models/DatabaseRelated/BuildingDBHandler.js
+++ Models/DatabaseRelated/BuildingDBHandler.js
@@ -5,17 +5,19 @@ import GeneralDBHelper from "./GeneralDBHelper";
 
 class BuildingDBHandler {
     constructor() {}
-    static async list_all_buildings(authToken = "WanNeng") {
-        return GeneralDBHelper.GET_REQUEST(
+    static list_all_buildings(authToken = "WanNeng") {
+        const result = GeneralDBHelper.GET_REQUEST(
             DATABASE_GET_LIST_ALL_BUILDINGS,
             authToken
         );
+        return result
     }
-    static async list_all_buildings_like(pattern, authToken = "WanNeng") {
-        return GeneralDBHelper.GET_REQUEST(
+    static list_all_buildings_like(pattern, authToken = "WanNeng") {
+        const result = GeneralDBHelper.GET_REQUEST(
           DATABASE_GET_LIST_ALL_BUILDINGS + pattern,
             authToken
         );
+        return result
     }
 }
 
