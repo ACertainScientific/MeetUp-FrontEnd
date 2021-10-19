f8337ce32c08d0cff39534e0a201a7f843a9f403
yichenp0104 <liyichen20000104@outlook.com>
2021-10-19 17:40:02 -0400

    Update UserDBHandler

--- Models/DatabaseRelated/UserDBHandler.js
+++ Models/DatabaseRelated/UserDBHandler.js
@@ -2,11 +2,13 @@ import { DATABASE_GET_LIST_USER } from "../../Constants/DatabaseConfig";
 import GeneralDBHelper from "./GeneralDBHelper";
 
 class UserDBHandler {
-    constructor() {
-      
-    }
-    static async get_user(){
-      return GeneralDBHelper.GET_REQUEST(DATABASE_GET_LIST_USER)
+    constructor() {}
+
+    static get_user(){
+      const result = GeneralDBHelper.GET_REQUEST(
+        DATABASE_GET_LIST_USER
+        )
+      return result;
     }
     
   }
