99554c139ad7210ba828c540e3c898867640e4de
JackHKJ <kjhe217@gmail.com>
2021-10-29 17:40:33 -0400

    Shown room status

--- screens/ContentScreens/RoomStatusPage.js
+++ screens/ContentScreens/RoomStatusPage.js
@@ -5,38 +5,37 @@ import MeetUpNavBar from "../../Components/MeetUpNavBar";
 import ElevatedCard from "../../Components/PageLineupComponents/ElevatedCard";
 import THEME_COLOR from "../../Constants/Color";
 import RoomStatusLabel from "../../Components/PageLineupComponents/RoomStatusLabel";
-import { ROOM_STATUS_OCCUPIED, ROOM_STATUS_SANITIZED } from "../../Constants/RoomStatusConstants";
+import {
+    ROOM_STATUS_OCCUPIED,
+    ROOM_STATUS_SANITIZED,
+} from "../../Constants/RoomStatusConstants";
 import RoomDBHandler from "../../Models/DatabaseRelated/RoomDBHandler";
 
 const RoomStatusPage = (param) => {
     // Fetcing room id by the navigation param
-    const roomId =  param.route.params.roomId
-    console.log("Fetched room id: " + roomId)
-    
-    // Const 
-    const [buildingId, setBuildingId] = useState("")
-    const [buildingName, setBuildingName] = useState("")
-    const [floor, setFloor] = useState("")
-    const [roomName, setRoomName] = useState("")
+    const roomId = param.route.params.roomId;
+    console.log("Fetched room id: " + roomId);
 
+    // Const
+    const [buildingId, setBuildingId] = useState("");
+    const [buildingName, setBuildingName] = useState("");
+    const [floor, setFloor] = useState("");
+    const [roomName, setRoomName] = useState("");
 
-    useEffect(()=>{
-        RoomDBHandler.detail_room(roomId, "WanNeng").then(
-            (response)=>{
-                console.log(response)
-                setBuildingId(response.buildingId)
-                setBuildingName(response.buildingName)
-                setFloor(response.floor)
-                setRoomName(response.name)
-            }
-        ).catch(
-            (error)=>{
-                console.error("Encountered error in fetching room.")
-                console.error(error)
-            }
-        )
-    })
-
+    useEffect(() => {
+        RoomDBHandler.detail_room(roomId, "WanNeng")
+            .then((response) => {
+                console.log(response);
+                setBuildingId(response.buildingId);
+                setBuildingName(response.buildingName);
+                setFloor(response.floor);
+                setRoomName(response.name);
+            })
+            .catch((error) => {
+                console.error("Encountered error in fetching room.");
+                console.error(error);
+            });
+    });
 
     return (
         <AutoResizableWindow>
@@ -67,11 +66,20 @@ const RoomStatusPage = (param) => {
                     }}
                 ></Button>
             </MeetUpNavBar>
-            <Text>This is the Room Status page</Text>
+            <View style={styles.roomInfo}>
+                <Text style={styles.roomInfoHint}>Status of Room </Text>
+                <Text style={styles.roomInfoHintHL}>{roomName}</Text>
+                <Text style={styles.roomInfoHint}> in Building </Text>
+                <Text style={styles.roomInfoHintHL}>{buildingName}</Text>
+                <Text style={styles.roomInfoHint}>, floor </Text>
+                <Text style={styles.roomInfoHintHL}>{floor}</Text>
+            </View>
             <View style={{ alignItems: "center", justifyContent: "center" }}>
                 <ElevatedCard>
                     <Text>Test CARD</Text>
-                    <RoomStatusLabel currentStatus={ROOM_STATUS_OCCUPIED}></RoomStatusLabel>
+                    <RoomStatusLabel
+                        currentStatus={ROOM_STATUS_OCCUPIED}
+                    ></RoomStatusLabel>
                 </ElevatedCard>
             </View>
         </AutoResizableWindow>
@@ -83,6 +91,24 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
+    },
+    roomInfo: {
+        display: "flex",
+        flexDirection: "row",
+        alignContent:"center",
+        justifyContent:"flex-end"
+    },
+    roomInfoHint: {
+        color: THEME_COLOR.subcolor,
+        fontSize: 20,
+        fontStyle: "italic",
+        fontWeight: "bold",
+    },
+    roomInfoHintHL: {
+        color: THEME_COLOR.main,
+        fontSize: 25,
+        fontStyle: "italic",
+        fontWeight: "bold",
     },
 });
 
